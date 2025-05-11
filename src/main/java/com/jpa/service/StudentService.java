7c345c16a642b68c2a781b19395643ff0199bba6
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2025-05-11 22:02:21 +0530

    Update StudentService.java

--- src/main/java/com/jpa/service/StudentService.java
+++ src/main/java/com/jpa/service/StudentService.java
@@ -1,54 +1,49 @@
 package com.jpa.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jpa.modal.Student;
 import com.jpa.repo.StudentRepo;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-public class StudentService
-{
+public class StudentService{
+
 	// Constructor dependency injection,
 	@Autowired
 	private StudentRepo studentRepo;
 	
 	// CRUD 
 	//1. INSERT INTO table_name VALUES();
-	public Student insertStudent(Student student)
-	{
+	public Student insertStudent(Student student){
 		Student st =  studentRepo.save(student);
 		log.info("Student {},saved to the database",student.getFaName());
 		return st;
 	}
 	
 	// 2. Select * from table_name,
-	public List<Student> getStudents()
-	{
+	public List<Student> getStudents(){
 		List<Student> students = studentRepo.findAll();
+  log.info("get Student list");
 		return students;
 	}
 	
 	// 3. findByFirstName
-	public Student getByName(String faName)
-	{
+	public Student getByName(String faName){
+  log.info("findByFirstName");
 		return studentRepo.findByFaName(faName);
 	}
 	
 	// 4. findByNameContaining
-	public List<Student> getByNameContainig(String name)
-	{
+	public List<Student> getByNameContainig(String name){
 		return studentRepo.findByFaNameContaining(name);
 	}
 	
 	// 5. findBylNameNotNull
-	public List<Student> getBylNameNotNull()
-	{
+	public List<Student> getBylNameNotNull(){
 		return studentRepo.findByLaNameNotNull();
 	}
 	
