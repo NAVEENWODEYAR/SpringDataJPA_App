b48def1cbcde7b80212d79c6fa78afaeec9e857e
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2026-08-17 06:11:04 +0530

    Update StudentService.java

--- src/main/java/com/jpa/service/StudentService.java
+++ src/main/java/com/jpa/service/StudentService.java
@@ -27,7 +27,7 @@ public class StudentService{
 	// 2. Select * from table_name,
 	public List<Student> getStudents(){
 		List<Student> students = studentRepo.findAll();
-  log.info("fetch Student list");
+  log.info("Fetching Student records");
 		return students;
 	}
 	
